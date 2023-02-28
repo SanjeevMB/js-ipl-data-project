72830fbfb1ea897d8a2cd9badf6cca482488d7c7
Sanjeev Kumar <sanjeev.kumar.23.2@mountblue.tech>
2023-03-01 00:34:42 +0530

    2-matches-won-per-team-per-year.cjs, 2-matches-won-per-team-per-year.json
    Number of matches won per team per year in IPL

---
+++ src/server/2-matches-won-per-team-per-year.cjs
@@ -0,0 +1,46 @@
+// Number of matches won per.team per year in IPL.
+
+const fs = require('fs');
+const path = require('path');
+const { parse } = require('csv-parse');
+
+let matchWon = [];
+
+let dataFilePath = './../../data/matches.csv';
+let writeFilePath = './../../public/output/2-matches-won-per-team-per-year.json'
+
+fs.createReadStream(path.join(__dirname, dataFilePath))
+    .pipe(
+        parse({
+            columns: true, dslimiter: ','
+        })).on('data', function (dataRow) {
+            let tempObj = {};
+
+            if (matchWon.length === 0) {
+                tempObj.team = dataRow.winner;
+                tempObj[dataRow.season] = 1;
+                matchWon.push(tempObj);
+            }
+
+            if(matchWon.some((item) => item.team === dataRow.winner)){
+                matchWon.map((element, index, array) => {
+                    if(element.team === dataRow.winner && element[dataRow.season] === undefined){
+                        element[dataRow.season] = 1;
+                    }else if(element.team === dataRow.winner && element[dataRow.season] !== undefined){
+                        element[dataRow.season] += 1;
+                    }
+                })
+            }else{
+                tempObj.team = dataRow.winner;
+                tempObj[dataRow.season] = 1;
+                matchWon.push(tempObj);                
+            }
+
+
+        }).on('end', function () {
+            fs.writeFile(path.join(__dirname, writeFilePath), JSON.stringify(matchWon), function (error) {
+                if (error) {
+                    console.log(error);
+                }
+            })
+        })
