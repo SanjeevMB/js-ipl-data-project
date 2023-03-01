345841c9180c496a5bcf783cc8a0d22e4c01c51e
Sanjeev Kumar <sanjeev.kumar.23.2@mountblue.tech>
2023-03-01 12:56:55 +0530

    3-extra-runs-per-team-in-2016.cjs, 3-extra-runs-per-team-in-2016.json
    Extra runs conceded per team in the year 2016

---
+++ src/server/3-extra-runs-per-team-in-2016.cjs
@@ -0,0 +1,46 @@
+// Extra runs conceded per team in the year 2016
+
+const fs = require('fs');
+const path = require('path');
+const { parse } = require('csv-parse');
+
+function main() {
+    
+    let matches = [];
+    let extraRunsStore = {};
+
+    let matchFilePath = './../../data/matches.csv';
+    let deliveryFilePath = './../../data/deliveries.csv';
+    let targetFilePath = './../../public/output/3-extra-runs-per-team-in-2016.json';
+
+    fs.createReadStream(path.join(__dirname, matchFilePath))
+        .pipe(
+            parse({
+                columns: true, dslimiter: ','
+            })).on('data', function (matchData) {
+                if (matchData.season == 2016) {
+                    matches.push(matchData);
+                }
+            }).on('end', function () {
+                fs.createReadStream(path.join(__dirname, deliveryFilePath))
+                    .pipe(parse({
+                        columns: true, delimiter: ','
+                    })).on('data', function (deliveryData) {
+                        if (matches.some((item) => item.id == deliveryData.match_id)) {
+                            if (extraRunsStore[deliveryData.bowling_team] == undefined) {
+                                extraRunsStore[deliveryData.bowling_team] = +(deliveryData.extra_runs);
+                            } else if (extraRunsStore[deliveryData.bowling_team] != undefined) {
+                                extraRunsStore[deliveryData.bowling_team] += +(deliveryData.extra_runs);
+                            }
+                        }
+                    }).on('end', function () {
+                        fs.writeFile(path.join(__dirname, targetFilePath), JSON.stringify(extraRunsStore), function (error) {
+                            if (error) {
+                                console.log(error);
+                            }
+                        })
+                    })
+            })
+}
+
+main();
