8203cb0690f80a926da8dc555fda07edcbc23de2
Sanjeev Kumar <sanjeev.kumar.23.2@mountblue.tech>
2023-03-04 13:47:49 +0530

    Extra runs per team in 2016

--- public/index.cjs
+++ public/index.cjs
@@ -1,3 +1,4 @@
+
 fetch('./output/1-matches-per-year.json')
   .then((data) => data.json())
   .then((data) => {
@@ -62,4 +63,73 @@ fetch('./output/1-matches-per-year.json')
       }
 
     });
-  })
+  });
+
+
+
+fetch('./output/3-extra-runs-per-team-in-2016.json')
+.then((data) => data.json())
+.then((data) => {
+  let extraRuns = Object.values(data);
+
+  Highcharts.chart('container3', {
+
+    title: {
+      text: 'Indian premier league',
+      align: 'left'
+    },
+
+    subtitle: {
+      align: 'left'
+    },
+
+    yAxis: {
+      title: {
+        text: 'Extra runs per team in 2016'
+      }
+    },
+
+    xAxis: {
+      accessibility: {
+        rangeDescription: 'Range: 2008 to 2020'
+      }
+    },
+
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle'
+    },
+
+    plotOptions: {
+      series: {
+        label: {
+          connectorAllowed: false
+        },
+        pointStart: 2008
+      }
+    },
+
+    series: [{
+      name: 'Extra runs per team in 2016',
+      data: extraRuns
+    }],
+
+    responsive: {
+      rules: [{
+        condition: {
+          maxWidth: 500
+        },
+        chartOptions: {
+          legend: {
+            layout: 'horizontal',
+            align: 'center',
+            verticalAlign: 'bottom'
+          }
+        }
+      }]
+    }
+
+  });
+});
+
